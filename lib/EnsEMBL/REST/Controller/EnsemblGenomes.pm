10a751645f7e98daf3c0ac3cc4ebeea3ba21d0f5
dstaines <dstaines@ebi.ac.uk>
2013-11-06 15:51:45 +0000

    added docs

--- lib/EnsEMBL/REST/Controller/EnsemblGenomes.pm
+++ lib/EnsEMBL/REST/Controller/EnsemblGenomes.pm
@@ -7,23 +7,26 @@ use Bio::EnsEMBL::EGVersion;
 require EnsEMBL::REST;
 EnsEMBL::REST->turn_on_config_serialisers(__PACKAGE__);
 
-BEGIN {extends 'Catalyst::Controller::REST'; }
+BEGIN { extends 'Catalyst::Controller::REST'; }
 
-=pod
+__PACKAGE__->config(
+  default => 'application/json',
+  map     => {
+		  'application/json' => [],
+		  'text/x-yaml'      => [],
+		  'text/xml'         => [],
+		  'text/plain'       => ['YAML'],});
 
-/lookup/genome/escherichia_coli_str_k_12_substr_mg1655
-
-application/json
-http://localhost:3000/genome/genome_name?content-type=application/json
-=cut
-
-sub genome : Chained('/') PathPart('lookup/genome') : ActionClass('REST') :Args(1) { }
+sub genome : Chained('/') PathPart('lookup/genome') :
+  ActionClass('REST') : Args(1) { }
 
 sub genome_GET {
   my ($self, $c, $genome) = @_;
   $c->log()->info("Getting DBA for $genome");
   my $dba = $c->model('Registry')->get_DBAdaptor($genome, 'core', 1);
-  $c->go('ReturnError', 'custom', ["Could not fetch adaptor for $genome"]) unless $dba;
+  $c->go('ReturnError', 'custom',
+		 ["Could not fetch adaptor for $genome"])
+	unless $dba;
   $c->log()->info("Exporting genes for $genome");
   my $genes = Bio::EnsEMBL::GenomeExporter->export_genes($dba);
   $c->log()->info("Finished exporting genes for $genome");
@@ -31,24 +34,15 @@ sub genome_GET {
   return;
 }
 
-=pod
-
-/info/eg_version
-
-application/json
-http://localhost:3000/genome/eg_version
-=cut
-
-sub ensgen_version : Chained('/') PathPart('info/eg_version') : ActionClass('REST') :Args(0) { }
+sub ensgen_version : Chained('/') PathPart('info/eg_version') :
+  ActionClass('REST') : Args(0) { }
 
 sub ensgen_version_GET {
   my ($self, $c) = @_;
   $c->log()->info("Retrieving EG version from registry");
   # lazy load the registry
   $c->model('Registry')->_registry();
-  $self->status_ok($c, entity => {
-  	version=>eg_version()
-  });
+  $self->status_ok($c, entity => {version => eg_version()});
   return;
 }
 
